11e7e452fb612907d88bf27f7dd9f56910268edb
kezhang <zhangke918525@gmail.com>
2025-09-22 12:06:07 -0600

    Practice 2

--- Chapter02_Variables_and_Data_Types/code/code2-3-variable_data_types.py
+++ Chapter02_Variables_and_Data_Types/code/code2-3-variable_data_types.py
@@ -1,3 +1,5 @@
-a = '1234asd'
-print(type(a))
-print(isinstance(a, int))
+pi = 3.1415927
+
+print("Type of pi: %s" %(type(pi)))
+
+print("Is pi an instance of integer? %s" %(isinstance(pi,int)))
--- Chapter02_Variables_and_Data_Types/code/code2-8-data_type_conversion.py
+++ Chapter02_Variables_and_Data_Types/code/code2-8-data_type_conversion.py
@@ -3,57 +3,63 @@
 # 纯数字的字符串
 s = '2024'
 n = int(s)
-print(type(s), type(n))
+print("Type of s: %s, type of n: %s"%(type(s),type(n)))
 # 浮点数float-->整数int
 s1 = 2.23
-print(int(s1))
+print("s1 = %f"%s1)
+print("s1 will be converted to int: %d"%int(s1))
 # 布尔bool-->整数int
 s2, s3 = True, False
-print(int(s2),int(s3))
+print("s2 = %s, s3 = %s"%(s2,s3))
+print("s2 and s3 will be converted to int: %d, %d"%(int(s2),int(s3)))
 
+print("*" * 20)
 
 # 转换为浮点数float
 # str-->float
 s = '324.6' # 有没有小数点都可以
-print(float(s))
+print("s = %s"%s)
+print("s will be converted to float: %f"%float(s))
 # int-->float
 n = 2024
-print(float(n))
+print("n = %d"%n)
+print("n will be converted to float: %f"%float(n))
 # bool-->float
-print(float(s2), float(s3))
+print("s2 = %s, s3 = %s"%(s2,s3))
+print("s2 and s3 will be converted to float: %f, %f"%(float(s2),float(s3)))
 
-
+print('*'*20)
 
 # 转换为布尔bool
 # str-->bool
 s = '0'
-print(bool(s))
+print("s is a string contain something, even though it's '0',s = %s"%s)
+print("s will be converted to bool: %s"%bool(s))
 s1 = ''  # 空串
-print('*'*20)
-print(bool(s1))
+print("s1 is an empty string, s1 = %s"%s1)
+print("s1 will be converted to bool, s1 = %s"%s1)
 # int-->bool
 n=0
-print(bool(n))
+print("n is a number, n = %d"%n)
+print("n will be converted to bool: %s"%bool(n))
 # float-->bool
 f=0.0
-print(bool(f))
+print("f is a float number, f = %f"%f)
+print("f will be converted to bool: %s"%bool(f))
 
-
+print("*"*20)
 
 # 转换为字符串str
 # int-->str
 n = 5
-print(str(n))
-print(type(str(n)))
+print("n is a int, n = %d, after converted to str, n = %s, so now the type of n is %s"%(n,str(n),type(str(n))))
 # float -->str
 f = 5.3
-print(str(f))
-print(type(str(f)))
+print("f is a float, f = %f, after converted to str, f = %s, so now the type of f is %s"%(f,str(f),type(str(f))))
 # bool --> str
 a = True
-print(type(a))
-print(type(str(a)))
+print("a is a bool, a = %s, after converted to str, a = %s, so now the type of a is %s"%(a,str(a),type(str(a))))
 
 # 进制的转换
 s = '1a'
-print(int(s, 16))
+print("s is a string, s = %s, after converted to in with pase 16, s = %d"%(s,int(s,16)))
